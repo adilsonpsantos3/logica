c2311ffe006a7090d7164b76b1502fe33b792f3e
adilsonpsantos3 <57467825+adilsonpsantos3@users.noreply.github.com>
2020-08-04 23:00:17 -0300

    Add files via upload

---
+++ aula_1/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package Aula_java;
+
+/**
+ * @author Adilson Santos
+ * Hello World
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
