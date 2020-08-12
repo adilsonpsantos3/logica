80a20314a758683b77aeb930f4a9b0b72f1aca83
adilsonpsantos3 <57467825+adilsonpsantos3@users.noreply.github.com>
2020-08-12 00:01:44 -0300

    Add files via upload

---
+++ aula_7/CalculadoraMedia.java
@@ -0,0 +1,43 @@
+package exercicios;
+
+import java.util.Scanner;
+/**
+ * @author Adilson Santos
+ *
+ */
+public class CalculadoraMedia {
+
+	public static void main(String[] args) {
+		// variáveis
+        String nome;
+        double nota1,nota2,media;
+        //objetos
+        Scanner teclado = new Scanner(System.in);
+        //Entrada
+        System.out.print("Digite o seu nome:");
+        nome = teclado.nextLine();
+        System.out.print("Digite a nota1:");
+        nota1 = teclado.nextDouble();
+        System.out.print("Digite a nota2:");
+        nota2 = teclado.nextDouble();
+        //Processamento
+        media = (nota1 + nota2) /2;
+        //saida
+        System.out.println("======================");
+        System.out.println("== Cálculo da Média ==");
+        System.out.println("======================");
+        System.out.println("");
+        System.out.println("Nome:" + nome);
+        System.out.println("Média: " + media);
+        //estrutura de controle
+        if(media < 5) {
+        	System.out.println("REPORVADO");
+        } else {
+        	System.out.println("APROVADO");
+        }
+        teclado.close();
+        
+        
+	}
+
+}
