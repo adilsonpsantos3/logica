95f304f8121d7a6f6dbc373d85f5825f97ea6977
adilsonpsantos3 <57467825+adilsonpsantos3@users.noreply.github.com>
2020-08-16 01:21:21 -0300

    Add files via upload

---
+++ aula_8/Dado.java
@@ -0,0 +1,56 @@
+package exercicios;
+
+public class Dado {
+
+	public static void main(String[] args) {
+		//Adilson
+		int face = (int) (Math.random() * 6 + 1);//0 1 2 3 4 5( soma + 1)
+		System.out.println(face);
+		switch(face) {
+		case 1:
+		System.out.println("╔═════════╗");
+		System.out.println("║         ║");
+		System.out.println("║    ○    ║");
+		System.out.println("║         ║");
+		System.out.println("╚═════════╝");
+		break;
+		case 2:
+		System.out.println(" ╔═════════╗");
+		System.out.println(" ║○        ║");
+		System.out.println(" ║         ║");
+		System.out.println(" ║        ○║");
+		System.out.println(" ╚═════════╝");
+		break;
+		case 3:
+		System.out.println(" ╔═════════╗");
+		System.out.println(" ║○        ║");
+		System.out.println(" ║    ○    ║");
+		System.out.println(" ║        ○║");
+		System.out.println(" ╚═════════╝");
+		break;
+		case 4:
+		System.out.println(" ╔═════════╗");
+		System.out.println(" ║○      ○ ║");
+		System.out.println(" ║         ║");
+		System.out.println(" ║○      ○ ║");
+		System.out.println(" ╚═════════╝");
+		break;
+		case 5:
+		System.out.println("  ╔═════════╗");
+		System.out.println("  ║○       ○║");
+		System.out.println("  ║    ○    ║");
+		System.out.println("  ║○       ○║");
+		System.out.println("  ╚═════════╝");
+		break;
+		case 6:
+		System.out.println("  ╔═════════╗");
+		System.out.println("  ║○       ○║");
+		System.out.println("  ║○       ○║");
+		System.out.println("  ║○       ○║");
+		System.out.println("  ╚═════════╝");
+		break;
+		}
+
+	}
+
+}
