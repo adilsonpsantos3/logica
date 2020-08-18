05040c7d1d3d86a57100c8a30919ce0ef01c6be5
adilsonpsantos3 <57467825+adilsonpsantos3@users.noreply.github.com>
2020-08-17 23:22:26 -0300

    Add files via upload

---
+++ aula_9/AlcoolGasolina.java
@@ -0,0 +1,25 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class AlcoolGasolina {
+
+	public static void main(String[] args) {
+		// Adilson
+double alcool,gasolina;
+Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Álcool x Gasolina");
+		System.out.print("Digite o valor do álcool: ");
+		alcool = teclado.nextDouble();
+		System.out.print("Digite o valor da gasolina: ");
+		gasolina = teclado.nextDouble();
+		//processamento + saida
+		if (alcool < 0.7 * gasolina) {
+		System.out.println("ABASTEÇA COM ÁLCOOL");	
+		} else {
+		System.out.println("ABASTEÇA COM GASOLINA");
+		teclado.close();
+	}
+	}
+}
